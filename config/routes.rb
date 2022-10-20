e67573124b3f97c66fb6baa534a5f8022e1e388c
Enrique Ramirez <nrqrmz@gmail.com>
2022-10-20 01:03:59 -0500

    create pet model

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # As a user I can see all found pets
+  # As a user I can see details about one found pet (when and where it was found)
+  # As a user I can add a pet I found
+  # As a user I can update a pet
+  # As a user I can delete a pet
 end
