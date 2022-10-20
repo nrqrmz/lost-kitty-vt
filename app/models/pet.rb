09a42e69496655a3a81408405fa7c90294d93fc2
Enrique Ramirez <nrqrmz@gmail.com>
2022-10-20 01:47:23 -0500

    add found_days_ago method and constant SPECIES

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
+# this pet has a custom method and a constant
 class Pet < ApplicationRecord
+  SPECIES = %w[dog cat rabbit snake turtle].freeze
+
   validates :name, presence: true
-  validates :species, inclusion: {in: %w[dog cat rabbit snake turtle]}
+  validates :species, inclusion: {in: SPECIES}
+
+  def found_days_ago
+    "found #{(Date.today - found_on).to_i} days ago"
+  end
 end
