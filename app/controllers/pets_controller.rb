4a9355e10c103fc938a37472412da343cc20102a
Enrique Ramirez <nrqrmz@gmail.com>
2022-10-20 15:57:09 -0500

    enable to update

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -18,6 +18,17 @@ class PetsController < ApplicationController
     redirect_to pet_path(@pet)
   end
 
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    @pet.update(pet_params)
+
+    redirect_to pet_path(@pet)
+  end
+
   private
 
   def pet_params
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   get 'pets/new', to: 'pets#new', as: :new_pet
   post 'pets', to: 'pets#create'
   get 'pets/:id', to: 'pets#show', as: :pet
-  
+  get 'pets/:id/edit', to: 'pets#edit', as: :edit_pet
+  patch 'pets/:id', to: 'pets#update'
 
   # As a user I can see all found pets ###########
   # As a user I can see details about one found pet (when and where it was found) ###########
